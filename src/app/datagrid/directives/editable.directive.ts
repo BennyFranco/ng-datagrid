909b4867ff2959bef706927158d70d1e02b590ef
Benny Franco Dennis <bfranco@novutek.com>
2017-04-12 10:18:49 -0700

    Escape and enter keys mapped

--- src/app/datagrid/directives/editable.directive.ts
+++ src/app/datagrid/directives/editable.directive.ts
@@ -14,18 +14,27 @@ export class EditableDirective {
   @HostListener('document:click', ['$event']) onClickOut(event) {
     const clickedInside = this._elementRef.nativeElement.contains(event.target);
     if (!clickedInside) {
+      this.cancelCellEdition(true);
       this._elementRef.nativeElement.style.border = '1px solid #ddd';
-
-      if (this._elementRef.nativeElement.children.length > 1) {
-        this._elementRef.nativeElement.children[0].textContent = this._elementRef.nativeElement.children[1].value;
-        this._elementRef.nativeElement.removeChild(this._elementRef.nativeElement.children[1]);
-        this._elementRef.nativeElement.children[0].style.display = 'inherit';
-      }
     }
   }
 
   @HostListener('click') onclick() {
     this._elementRef.nativeElement.style.border = '2px solid blue';
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyboardInput(event: KeyboardEvent) {
+    console.log(event);
+
+    switch (event.key) {
+      case 'Escape':
+        this.cancelCellEdition(false);
+        break;
+      case 'Enter':
+        this.cancelCellEdition(true);
+        break;
+    }
   }
 
   private addInput() {
@@ -40,5 +49,16 @@ export class EditableDirective {
     input.style.border = 'none';
     input.style.width = '60px';
     input.focus();
+  }
+
+  private cancelCellEdition(saveElement: boolean) {
+
+    if (this._elementRef.nativeElement.children.length > 1) {
+      if (saveElement) {
+        this._elementRef.nativeElement.children[0].textContent = this._elementRef.nativeElement.children[1].value;
+      }
+      this._elementRef.nativeElement.removeChild(this._elementRef.nativeElement.children[1]);
+      this._elementRef.nativeElement.children[0].style.display = 'inherit';
+    }
   }
 }
