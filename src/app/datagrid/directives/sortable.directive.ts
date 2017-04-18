828dbfc3f7cf57f1b83ed4d50e96ace6f700e1d6
Benny Franco Dennis <bfranco@novutek.com>
2017-04-17 17:01:43 -0700

    Selected cell squad removed when sorting

--- src/app/datagrid/directives/sortable.directive.ts
+++ src/app/datagrid/directives/sortable.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, Input, HostListener, ElementRef } from '@angular/core';
+import { DatagridService } from '../datagrid.service';
 
 @Directive({
   selector: '[ngSortable]'
@@ -9,24 +10,53 @@ export class SortableDirective {
   private column: number;
   private isAsc = false;
 
-  constructor(private _elementRef: ElementRef) {
+  constructor(
+    private _elementRef: ElementRef,
+    private datagridService: DatagridService) {
     this._elementRef.nativeElement.className = 'sortable-header ';
   }
 
   @HostListener('click') onClick() {
+    this.removeSelection(this.datagridService.selectedElementId);
     this.column = this._elementRef.nativeElement.id;
     this.data.sort((a, b) => this.sortData(a, b));
     this.isAsc = this.isAsc ? false : true;
   }
 
   sortData(data1: any, data2: any) {
-    console.log(this.column);
     if (data1[this.column] === data2[this.column]) {
       return 0;
     } else if (!this.isAsc) {
       return (data1[this.column] < data2[this.column]) ? -1 : 1;
     } else {
       return (data1[this.column] > data2[this.column]) ? -1 : 1;
+    }
+  }
+
+  private selectElement(id?: string) {
+    if (id) {
+      const element = document.getElementById(id);
+      element.className = 'selected';
+      this.datagridService.selectedElement = element;
+      this.datagridService.selectedElementId = element.id;
+    }
+  }
+
+  private removeSelection(id?: string) {
+    if (id) {
+      const element = document.getElementById(id);
+      element.className = '';
+      this.cancelCellEdition(element, false);
+    }
+  }
+
+  private cancelCellEdition(element, saveElement: boolean) {
+    if (element.children.length > 1) {
+      if (saveElement) {
+        element.children[0].textContent = element.children[1].value;
+      }
+      element.removeChild(element.children[1]);
+      element.children[0].style.display = 'inherit';
     }
   }
 }
